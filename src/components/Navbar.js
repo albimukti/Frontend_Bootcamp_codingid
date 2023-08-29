84f03e693c65772f7abf085647e8d03cbf58604c
Naufal Abrori <nopal.abrori@gmail.com>
2023-08-29 09:04:01 +0700

    Initial commit dari Naufal fitur register

---
+++ src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Stack, Button, Typography, IconButton } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const Navbar = () => {
+
+  const whiteTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#FFFFFF'
+      },
+    },
+  });
+
+  const primary = createTheme({
+    palette: {
+      primary: {
+        main: '#5B4947'
+      },
+    },
+  });
+
+  const secondary = createTheme({
+    palette: {
+      primary: {
+        main: '#FABC1D'
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={whiteTheme}>
+      <AppBar position='static'>
+      <Toolbar>
+        <Link to='/'>
+          <IconButton size='large' edge='start' aria-label='logo'>
+            <img src='/images/logo.png' alt='logo'/>
+          </IconButton>
+        </Link>
+        <Typography component='div' sx={{ flexGrow: 1 }}></Typography>
+        <Stack direction='row' spacing={2}>
+          <ThemeProvider theme={primary}>
+            <Button sx={{px:5, borderRadius:2}} variant='outlined' style={{color:'black'}}>Login</Button>
+          </ThemeProvider>
+          <ThemeProvider theme={secondary}>
+            <Link to='/register'>
+              <Button sx={{px:5, borderRadius:2}} variant='contained'>Register</Button>
+            </Link>
+          </ThemeProvider>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+    </ThemeProvider>
+)
+}
+
+export default Navbar
